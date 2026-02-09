22c03b1060b385a1aa63fb56f4b96129bf23764b
Drake <coldshort@gmail.com>
2026-02-09 19:48:01 +0700

    bucket & cluster name update

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "iac-drake"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-southeast-7"
   }
 
   required_version = "~> 1.6.3"
